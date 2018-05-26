3db0784220b8c413f904ffada42cb54c4062db39
Pascal Seitz <pascal.seitz@gmail.com>
2018-05-26 10:24:20 +0200

    initial

---
+++ src/hasher.rs
@@ -0,0 +1,130 @@
+use std::default::Default;
+use std::hash::{Hasher, BuildHasherDefault};
+use std::collections::{HashMap, HashSet};
+use std::ptr::copy_nonoverlapping;
+
+
+/// An implementation of the Fowler–Noll–Vo hash function.
+///
+/// See the [crate documentation](index.html) for more details.
+#[allow(missing_copy_implementations)]
+#[derive(Debug, Clone)]
+pub struct FnvYoshiHasher(u32);
+impl Default for FnvYoshiHasher {
+    #[inline]
+    fn default() -> FnvYoshiHasher {
+        FnvYoshiHasher(0xD8AFFD71)
+    }
+}
+
+impl FnvYoshiHasher {
+    /// Create an FNV hasher starting with a state corresponding
+    /// to the hash `key`.
+    #[inline]
+    pub fn with_key(key: u32) -> FnvYoshiHasher {
+        FnvYoshiHasher(key)
+    }
+}
+
+impl Hasher for FnvYoshiHasher {
+    #[inline]
+    fn finish(&self) -> u64 {
+        self.0 as u64
+    }
+
+    #[inline(always)]
+    fn write(&mut self, bytes: &[u8]) {
+        let FnvYoshiHasher(mut hash) = *self;
+        hash = fnv32a_yoshimitsu_triad(hash, bytes);
+        *self = FnvYoshiHasher(hash);
+    }
+}
+
+/// A builder for default FNV hashers.
+pub type FnvYoshiBuildHasher = BuildHasherDefault<FnvYoshiHasher>;
+
+/// A `HashMap` using a default FNV hasher.
+pub type FnvYoshiHashMap<K, V> = HashMap<K, V, FnvYoshiBuildHasher>;
+
+
+#[inline]
+fn rotl32(x: u32, r: u8) -> u32
+{
+  (x << r) | (x >> (32 - r))
+}
+
+#[inline]
+fn read_u32_p(p: *const u8) -> u32{
+    let mut out: u32 = 0;
+    unsafe {
+        copy_nonoverlapping(p, &mut out as *mut u32 as *mut u8, 4);
+    }
+    out
+}
+
+#[inline]
+fn read_u16_p(p: *const u8) -> u16{
+    let mut out: u16 = 0;
+    unsafe {
+        copy_nonoverlapping(p, &mut out as *mut u16 as *mut u8, 2);
+    }
+    out
+}
+
+#[inline(always)]
+pub fn fnv32a_yoshimitsu_hasher(bytes: &[u8]) -> u32 {
+    fnv32a_yoshimitsu_triad(0xD8AFFD71, bytes)
+}
+
+#[inline(always)]
+pub fn fnv32a_yoshimitsu_triad(seed:u32, bytes: &[u8]) -> u32
+{
+    let mut len: u32 = bytes.len() as u32;
+    let mut p: *const u8 = bytes.as_ptr();
+
+    const PRIME:u32 = 709607;
+    let mut hash32_a: u32 = seed ^ 2166136261;
+    let mut hash32_b: u32 = 2166136261 + len;
+    let mut hash32_c: u32 = 2166136261;
+    while len >= 24{
+        hash32_a = (hash32_a ^ (rotl32(read_u32_p(p), 5)  ^ read_u32_p(unsafe{p.offset(4)})))  .wrapping_mul(PRIME);
+        hash32_b = (hash32_b ^ (rotl32(read_u32_p(unsafe{p.offset(8)}), 5)  ^ read_u32_p(unsafe{p.offset(12)}))) .wrapping_mul(PRIME);
+        hash32_c = (hash32_c ^ (rotl32(read_u32_p(unsafe{p.offset(16)}), 5) ^ read_u32_p(unsafe{p.offset(20)}))) .wrapping_mul(PRIME);
+        len-= 24;
+        p = unsafe{p.offset(24)};
+    }
+
+    if p == bytes.as_ptr() {
+        hash32_a = (hash32_a ^ rotl32(hash32_c, 5)) .wrapping_mul(PRIME);
+    }
+    //Cases 0. .31
+    if (len & 16) != 0{
+        hash32_a = (hash32_a ^ (rotl32(read_u32_p(p), 5) ^ read_u32_p(unsafe{p.offset(4)}))) .wrapping_mul(PRIME);
+        hash32_b = (hash32_b ^ (rotl32(read_u32_p(unsafe{p.offset(8)}), 5) ^ read_u32_p(unsafe{p.offset(12)}))) .wrapping_mul(PRIME);
+        p = unsafe{p.offset(16)};
+    }
+    //Cases 0. .15
+    if (len & 8) != 0 {
+        hash32_a = (hash32_a ^ read_u32_p(p)) .wrapping_mul(PRIME);
+        hash32_b = (hash32_b ^ read_u32_p(unsafe{p.offset(4)})) .wrapping_mul(PRIME);
+        p = unsafe{p.offset(8)};
+    }
+    //Cases:0. .7
+    if (len & 4) != 0 {
+        hash32_a = (hash32_a ^ read_u16_p(p) as u32) .wrapping_mul(PRIME);
+        hash32_b = (hash32_b ^ read_u16_p(unsafe{p.offset(2)}) as u32) .wrapping_mul(PRIME);
+        p = unsafe{p.offset(4)};
+    }
+    // //Cases:0. .3
+    if (len & 2) != 0 {
+        hash32_a = (hash32_a ^ read_u16_p(p) as u32) .wrapping_mul(PRIME);
+        p = unsafe{p.offset(2)};
+    }
+    if (len & 1) != 0 {
+        hash32_a = (hash32_a ^ unsafe{*p} as u32) .wrapping_mul(PRIME);
+    }
+    hash32_a = (hash32_a ^ rotl32(hash32_b, 5)) .wrapping_mul(PRIME);
+    hash32_a ^ (hash32_a >> 16)
+
+}
+
